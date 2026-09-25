da822ff73ff406d3e4d8974360d575e0848bd71f
GDS K S <gagandua078@gmail.com>
2026-09-25 00:32:41 -0500

    fix: add pipelines sync and triggers to CLI completion tree

--- cmd/levelrail-cli/completion.go
+++ cmd/levelrail-cli/completion.go
@@ -131,7 +131,7 @@ var cliCommandTree = map[string]*cmdNode{
 	"registry-credentials":  {subs: map[string]*cmdNode{"list": nil, "get": nil, "create": nil, "update": nil, "delete": nil, "test": nil, "repositories": nil, "tags": nil}},
 	"registry":              {subs: map[string]*cmdNode{"status": nil, "enable": nil, "disable": nil, "repositories": nil, "tags": nil}},
 	"flags":                 {subs: map[string]*cmdNode{"create": nil, "list": nil, "get": nil, "set": nil, "delete": nil}},
-	"pipelines":             {subs: map[string]*cmdNode{"list": nil, "validate": nil, "save": nil, "delete": nil, "run": nil, "runs": nil, "logs": nil, "cancel": nil, "approve": nil}},
+	"pipelines":             {subs: map[string]*cmdNode{"list": nil, "validate": nil, "save": nil, "delete": nil, "run": nil, "runs": nil, "logs": nil, "cancel": nil, "approve": nil, "sync": nil, "triggers": nil}},
 	"lb":                    {subs: map[string]*cmdNode{"show": nil, "set": nil, "clear": nil, "status": nil, "export": nil, "import": nil}},
 	"tags":                  {subs: map[string]*cmdNode{"list": nil, "create": nil, "delete": nil, "apps": nil}},
 	"nodes": {subs: map[string]*cmdNode{
